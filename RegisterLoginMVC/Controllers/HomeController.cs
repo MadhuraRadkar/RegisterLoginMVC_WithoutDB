b56507fcb7f830391344fef1fa8ec2262e009788
MadhuraRadkar <142924000+MadhuraRadkar@users.noreply.github.com>
2024-03-10 14:22:27 +0530

    Add project files.

---
+++ RegisterLoginMVC/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegisterLoginMVC.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+
+namespace RegisterLoginMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private IHttpContextAccessor contextAccessor;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(Users users)
+        {
+            HttpContext.Session.SetString("email",users.Email);
+            HttpContext.Session.SetString("password", users.Password);
+            return RedirectToAction("Login");
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(Users users)
+        {
+            string email = HttpContext.Session.GetString("email");
+            string password = HttpContext.Session.GetString("password");
+            if(email==users.Email && password == users.Password)
+            {
+                ViewBag.Message = "Sucess";
+            }
+            else
+            {
+                ViewBag.Message = "Invalid Email or Password";
+            }
+
+            return View();
+        }
+    }
+}
---
+++ RegisterLoginMVC/Models/Users.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegisterLoginMVC.Models
+{
+    public class Users
+    {
+        public string? Username { get; set; }
+        [Required]
+        // [RegularExpression("^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:a-z0-9?\\\\.)+a-z0-9?$\r\n",ErrorMessage ="Email id should be in proper format")]
+
+        // [RegularExpression("^[0-9A-Z]([-.\\w]*[0-9A-Z])*$",ErrorMessage ="Email should be in proper format")]
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password {  get; set; }
+        [Required]
+        public string? ConfirmPass {  get; set; }
+    }
+}
